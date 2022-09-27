0ee6e789e16fea7eab6b1c0fdf3e2b1d29f50c47
DimaGershchuk <dimagersuk29@gmail.com>
2022-09-27 10:17:48 +0300

    Початкові налаштування

--- OSU/Form1.cs
+++ OSU/Form1.cs
@@ -12,15 +12,27 @@ namespace OSU
 {
     public partial class Form1 : Form
     {
+        public Bitmap Handlertexure = Resource1.Handler, TargetTexture = Resource1.Target;
         public Form1()
         {
             InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+
+            UpdateStyles();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            int a;
-            int b;
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            var localPosition = this.PointToClient(Cursor.Position);
+            g.DrawImage(Handlertexure, new Rectangle(localPosition.X, localPosition.Y, 100, 100));
+            g.DrawImage(TargetTexture, new Rectangle(0, 0, 100, 100));
+
         }
     }
 }
