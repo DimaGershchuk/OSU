05f690a077ef8b2832792aba522ab8f5d5669129
unknown <you@example.com>
2022-09-27 12:30:32 +0300

    Last save

--- OSU/Form1.cs
+++ OSU/Form1.cs
@@ -13,6 +13,9 @@ namespace OSU
     public partial class Form1 : Form
     {
         public Bitmap Handlertexure = Resource1.Handler, TargetTexture = Resource1.Target;
+
+        private Point _targetPosition = Point.Empty;
+        private Point _direction = Point.Empty;
         public Form1()
         {
             InitializeComponent();
@@ -26,12 +29,23 @@ namespace OSU
             Refresh();
         }
 
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 500);
+        }
+
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
             var localPosition = this.PointToClient(Cursor.Position);
-            g.DrawImage(Handlertexure, new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100));
-            g.DrawImage(TargetTexture, new Rectangle(0, 0, 100, 100));
+            _targetPosition.X += _direction.X;
+            _targetPosition.X -= _direction.Y;
+            var handlerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100,100);
+            g.DrawImage(Handlertexure, handlerRect);
+            g.DrawImage(TargetTexture, targetRect);
+            
 
         }
     }
